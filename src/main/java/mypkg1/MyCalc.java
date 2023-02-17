4cf70a24f88b56bd3c9ba380a0fb49e47532336d
Anshika-04 <anshisaxena905@gmail.com>
2023-02-17 21:54:17 +0700

    first commit

---
+++ src/main/java/mypkg1/MyCalc.java
@@ -0,0 +1,15 @@
+package mypkg1;
+
+public class MyCalc {
+	public void show()
+	{
+		System.out.println("Hey Everyone , Welcome to Jenkins") ;
+	}
+    public static void main(String[] args) {
+		
+    	MyCalc ob = new MyCalc ();
+    	ob.show();
+
+	}
+
+}
